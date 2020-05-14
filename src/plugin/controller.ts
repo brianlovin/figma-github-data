9d439213e6ab5c50bedc0f0cd5db03ff9b817953
Brian Lovin <briandlovin@gmail.com>
2020-05-14 11:48:44 -0700

    Fix dialog height

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,15 +1,14 @@
 import populateSelectionWithData from './dataPopulator';
 
 async function main() {
-  const dataPopulatorVariableString = await 
-    figma.clientStorage.getAsync('dataPopulatorVariableString')
+  const dataPopulatorVariableString = await figma.clientStorage.getAsync('dataPopulatorVariableString');
 
-  figma.showUI(__html__, { width: 400, height: 108 });
+  figma.showUI(__html__, { width: 400, height: 148 });
   figma.ui.postMessage({
     dataPopulatorVariableString,
   });
 
-  figma.ui.onmessage = msg => {
+  figma.ui.onmessage = (msg) => {
     const { event, key, value } = msg;
     switch (event) {
       case 'STORE_KEY_VALUE':

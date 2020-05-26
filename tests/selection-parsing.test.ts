d5d99b90fe59503161dc5de3f0d9ce736ce124ce
Christian Giordano <christian@nuthinking.com>
2020-05-26 22:25:51 +0200

    put order in the test

--- tests/selection-parsing.test.ts
+++ tests/selection-parsing.test.ts
@@ -1,49 +1,8 @@
 import populateSelectionWithData from '../src/plugin/dataPopulator';
+import { issues } from './utils/data';
+import { createFrameWithSecondaryProperties, createTextLayer } from './utils';
 
 let requestCount = 0;
-
-const issues = [
-  {
-    created_at: '2020-05-26T02:35:56Z',
-    number: 5357,
-    title: 'Cannot login with all social media account',
-    user: {
-      login: 'itokun99',
-    },
-  },
-  {
-    created_at: '2020-05-20T16:29:00Z',
-    number: 5356,
-    title: 'Bump lint-staged from 3.6.1 to 10.2.4',
-    user: {
-      login: 'dependabot-preview[bot]',
-    },
-  },
-  {
-    created_at: '2020-05-15T19:34:49Z',
-    number: 5355,
-    title: 'Fix header style bug on login page',
-    user: {
-      login: 'edumoreira1506',
-    },
-  },
-  {
-    created_at: '2020-05-06T15:08:26Z',
-    number: 5348,
-    title: 'Bump react-helmet-async from 0.2.0 to 1.0.6',
-    user: {
-      login: 'dependabot-preview[bot]',
-    },
-  },
-  {
-    created_at: '2020-04-06T15:30:24Z',
-    number: 5338,
-    title: '[Security] Bump handlebars from 4.1.2 to 4.7.6',
-    user: {
-      login: 'dependabot-preview[bot]',
-    },
-  },
-];
 
 const figma: any = {
   lastNotification: '',
@@ -66,33 +25,6 @@ const figma: any = {
   currentPage: {},
 };
 (global as any).figma = figma;
-
-const createTextLayer = (name: string): any => {
-  return {
-    name,
-    type: 'TEXT',
-    characters: '',
-  };
-};
-
-const createFrameWithSecondaryProperties = (name: string): any => {
-  return {
-    name,
-    type: 'FRAME',
-    children: [
-      {
-        name: '__title',
-        type: 'TEXT',
-        characters: '',
-      },
-      {
-        name: 'Parent',
-        type: 'FRAME',
-        children: [createTextLayer('__user.login'), createTextLayer('__created_at'), createTextLayer('__number')],
-      },
-    ],
-  };
-};
 
 describe('Selection Parsing', () => {
   it('Should warn when there is no selection', async () => {
---
+++ tests/utils/data.ts
@@ -0,0 +1,42 @@
+export const issues = [
+  {
+    created_at: '2020-05-26T02:35:56Z',
+    number: 5357,
+    title: 'Cannot login with all social media account',
+    user: {
+      login: 'itokun99',
+    },
+  },
+  {
+    created_at: '2020-05-20T16:29:00Z',
+    number: 5356,
+    title: 'Bump lint-staged from 3.6.1 to 10.2.4',
+    user: {
+      login: 'dependabot-preview[bot]',
+    },
+  },
+  {
+    created_at: '2020-05-15T19:34:49Z',
+    number: 5355,
+    title: 'Fix header style bug on login page',
+    user: {
+      login: 'edumoreira1506',
+    },
+  },
+  {
+    created_at: '2020-05-06T15:08:26Z',
+    number: 5348,
+    title: 'Bump react-helmet-async from 0.2.0 to 1.0.6',
+    user: {
+      login: 'dependabot-preview[bot]',
+    },
+  },
+  {
+    created_at: '2020-04-06T15:30:24Z',
+    number: 5338,
+    title: '[Security] Bump handlebars from 4.1.2 to 4.7.6',
+    user: {
+      login: 'dependabot-preview[bot]',
+    },
+  },
+];

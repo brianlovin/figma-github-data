b3389b71edb815330fda1e13859cd56244560742
Christian Giordano <christian@nuthinking.com>
2020-05-26 19:47:24 +0200

    Added list test

--- tests/selection-parsing.test.ts
+++ tests/selection-parsing.test.ts
@@ -4,19 +4,44 @@ let requestCount = 0;
 
 const issues = [
   {
+    created_at: '2020-05-26T02:35:56Z',
+    number: 5357,
     title: 'Cannot login with all social media account',
+    user: {
+      login: 'itokun99',
+    },
   },
   {
+    created_at: '2020-05-20T16:29:00Z',
+    number: 5356,
     title: 'Bump lint-staged from 3.6.1 to 10.2.4',
+    user: {
+      login: 'dependabot-preview[bot]',
+    },
   },
   {
+    created_at: '2020-05-15T19:34:49Z',
+    number: 5355,
     title: 'Fix header style bug on login page',
+    user: {
+      login: 'edumoreira1506',
+    },
   },
   {
+    created_at: '2020-05-06T15:08:26Z',
+    number: 5348,
     title: 'Bump react-helmet-async from 0.2.0 to 1.0.6',
+    user: {
+      login: 'dependabot-preview[bot]',
+    },
   },
   {
+    created_at: '2020-04-06T15:30:24Z',
+    number: 5338,
     title: '[Security] Bump handlebars from 4.1.2 to 4.7.6',
+    user: {
+      login: 'dependabot-preview[bot]',
+    },
   },
 ];
 
@@ -42,6 +67,33 @@ const figma: any = {
 };
 (global as any).figma = figma;
 
+const createTextLayer = (name: string): any => {
+  return {
+    name,
+    type: 'TEXT',
+    characters: '',
+  };
+};
+
+const createFrameWithSecondaryProperties = (name: string): any => {
+  return {
+    name,
+    type: 'FRAME',
+    children: [
+      {
+        name: '__title',
+        type: 'TEXT',
+        characters: '',
+      },
+      {
+        name: 'Parent',
+        type: 'FRAME',
+        children: [createTextLayer('__user.login'), createTextLayer('__created_at'), createTextLayer('__number')],
+      },
+    ],
+  };
+};
+
 describe('Selection Parsing', () => {
   it('Should warn when there is no selection', async () => {
     figma.currentPage.selection = [];
@@ -53,17 +105,31 @@ describe('Selection Parsing', () => {
     requestCount = 0;
     figma.currentPage.selection = [
       {
-        name: 'Frame 1',
+        name: 'Parent',
         type: 'FRAME',
-        children: [
-          {
-            name: '__title',
-            type: 'TEXT',
-            characters: '',
-          },
-        ],
+        children: [createFrameWithSecondaryProperties('Frame 1'), createFrameWithSecondaryProperties('Frame 2')],
       },
     ];
+    await populateSelectionWithData({ type: 'issue', variable: '' });
+    const frames = figma.currentPage.selection[0].children;
+
+    let titleNode = frames[0].children[0];
+    expect(titleNode.characters).toEqual('Cannot login with all social media account');
+    let usernameNode = frames[0].children[1].children[0];
+    expect(usernameNode.characters).toEqual('itokun99');
+    let dateNode = frames[0].children[1].children[1];
+    expect(dateNode.characters).toEqual('26 May');
+    let numberNode = frames[0].children[1].children[2];
+    expect(numberNode.characters).toEqual('#5357');
+
+    titleNode = frames[1].children[0];
+    expect(titleNode.characters).toEqual('Bump lint-staged from 3.6.1 to 10.2.4');
+    usernameNode = frames[1].children[1].children[0];
+    expect(usernameNode.characters).toEqual('dependabot-preview[bot]');
+    dateNode = frames[1].children[1].children[1];
+    expect(dateNode.characters).toEqual('20 May');
+    numberNode = frames[1].children[1].children[2];
+    expect(numberNode.characters).toEqual('#5356');
   });
 
   it('Should parse propertly', async () => {
@@ -72,13 +138,7 @@ describe('Selection Parsing', () => {
       {
         name: 'Frame 1',
         type: 'FRAME',
-        children: [
-          {
-            name: '__title',
-            type: 'TEXT',
-            characters: '',
-          },
-        ],
+        children: [createTextLayer('__title')],
       },
     ];
     await populateSelectionWithData({ type: 'issue', variable: '' });
